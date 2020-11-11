6781e681b955c986a32ff96785792048175d9ab0
黃筱涵 <amber20010408@gmail.com>
2020-11-11 13:17:39 +0800

    Update 3-8.cpp

--- 1111midPractice/3-8.cpp
+++ 1111midPractice/3-8.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 void Towers(int n, char a, char b, char c){
 	if(n > 0){
+		//先將n-1個盤子從a搬到b
 		Towers(n-1, a, c, b);
 		cout << "Move disk " << n << " from " << a << " to " << c << endl;
 		Towers(n-1, b, a, c);
